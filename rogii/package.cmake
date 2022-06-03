463f4b8a3e54b30009d053f4f1db1a15d742e3e0
Alexey Kostin <a.kostin@rogii.com>
2022-06-03 15:56:19 +0400

    fixed gmock pass under linux

--- rogii/package.cmake
+++ rogii/package.cmake
@@ -101,9 +101,9 @@ if(NOT TARGET google::mock)
             google::mock
             PROPERTIES
                 IMPORTED_LOCATION
-                    "${CMAKE_CURRENT_LIST_DIR}/lib/libgmock.a"
+                    "${CMAKE_CURRENT_LIST_DIR}/lib/gmock.a"
                 IMPORTED_LOCATION_DEBUG
-                    "${CMAKE_CURRENT_LIST_DIR}/lib/libgmockd.a"
+                    "${CMAKE_CURRENT_LIST_DIR}/lib/gmockd.a"
                 INTERFACE_INCLUDE_DIRECTORIES
                     "${CMAKE_CURRENT_LIST_DIR}/include"
         )
